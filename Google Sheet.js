692a15fca58ef84d55f4235266b3e002143ebed9
Jitesh Dubey <jiteshdubey001@gmail.com>
2024-07-09 03:17:14 +0530

    first commit

---
+++ Google Sheet.js
@@ -0,0 +1,11 @@
+// const scriptURL = 'https://script.google.com/macros/s/AKfycby5Rza9YFnEfminn53MkFRv_3dpVrcCTj4CEcno92Nd4g-rPGlwv6Yobg-v3leCTnCgGw/exec'
+
+// const form = document.forms['contact-form']
+
+// form.addEventListener('submit', e => {
+//   e.preventDefault()
+//   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+//   .then(response => alert("Thank you! your form is submitted successfully." ))
+//   .then(() => { window.location.reload(); })
+//   .catch(error => console.error('Error!', error.message))
+// })
